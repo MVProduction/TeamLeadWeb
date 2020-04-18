24255e2f5ac678a537571e798d7d9c8dc0a6918b
Grabli66 <grabli66@gmail.com>
2020-04-18 23:05:02 +0300

    Improve posts

--- src/controllers/index_controller.cr
+++ src/controllers/index_controller.cr
@@ -5,16 +5,37 @@ require "../services/quote_service"
 require "../services/user_service"
 require "../services/models/user_info_data"
 
+# Количество объявлений на одной странице
+PAGE_POST_COUNT = 20
+
+# Количество символов в тексте объявления
+TEXT_CHAR_COUNT = 200
+
 get "/" do |env|
   postService = PostService.instance  
   userService = UserService.instance
   quoteService = QuoteService.instance  
+  
+  # Запрашивает количество страниц
+  pageCount = postService.getPageCount(PAGE_POST_COUNT)
+
+  # Номер страницы
+  pageNumber = env.params.query["page"]?
+
+  # Если есть номер страницы и он больше единицы
+  if !pageNumber.nil?
+    pn = pageNumber.to_i32
+    postList = postService.getPostsByPage(pn, PAGE_POST_COUNT, TEXT_CHAR_COUNT)
+  else
+    postList = postService.getRecentPosts(PAGE_POST_COUNT, TEXT_CHAR_COUNT)
+  end
 
   indexView = TemplateFactory.instance.getTemplate("main/index_view.html")
   indexView.render(
     { 
       loginUser: userService.getUserInfoFromCookie(env),
-      postList: postService.getRecentPosts(20, 200),
+      postList: postList,
+      pageCount: pageCount,
       quote: quoteService.getRandomQuote()
     })
 end
--- src/services/post_service.cr
+++ src/services/post_service.cr
@@ -13,11 +13,31 @@ class PostService < BaseService
         @@instance
     end
 
+    # Возвращает количество страниц для количества объявлений postInPage
+    def getPageCount(postInPage : Int32) : Int64
+        resp = sendGet("/posts/getPostCount")
+        count = resp["postCount"].as_i64
+        return (count / postInPage).to_i64
+    end
+
     # Возвращает объявление по идентификатору
     def getPost(id : Int64) : PostItemData        
         resp = sendGet("/posts/getById/#{id}")
         post = resp["post"]
         PostItemData.fromJson(post)
+    end
+
+    # Разбивает все объявления на страницы и возвращает 
+    # Объявления в количестве postInPage по индексу pageIndex
+    def getPostsByPage(pageIndex : Int32, postInPage : Int32, textLen : Int32) : Array(PostItemData)
+        resp = sendGet("/posts/getByPage/#{pageIndex}/#{postInPage}?textLen=#{textLen}")
+        posts = resp["posts"]?.try &.as_a?
+
+        return Array(PostItemData).new unless posts
+
+        return posts.map { |x|
+            PostItemData.fromJson(x)
+        }
     end
 
     # Возвращает объявления от начального индекса с заданным колличеством
