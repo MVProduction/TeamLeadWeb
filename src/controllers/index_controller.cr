87a6bfa9418bb43c80561058c5e4c5c37014cbbb
Grabli66 <grabli66@gmail.com>
2020-05-04 19:46:31 +0300

    Improve paging

--- src/controllers/index_controller.cr
+++ src/controllers/index_controller.cr
@@ -6,7 +6,7 @@ require "../services/user_service"
 require "../services/models/user_info_data"
 
 # Количество объявлений на одной странице
-PAGE_POST_COUNT = 2
+PAGE_POST_COUNT = 4
 
 # Количество символов в тексте объявления
 TEXT_CHAR_COUNT = 200
@@ -17,8 +17,7 @@ get "/" do |env|
   quoteService = QuoteService.instance  
   
   # Номер страницы
-  pageId = env.params.query["page"]?.try &.to_i64 || 1_i64
-  p pageId
+  pageId = env.params.query["page"]?.try &.to_i64 || 1_i64  
   
   response = postService.getPostsByPage(
     page: pageId,
@@ -26,14 +25,17 @@ get "/" do |env|
     textLen: TEXT_CHAR_COUNT)
   
   pageCount = response[1]
-  
+  prevPageId = pageId - 1
+  nextPageId = pageId + 1
+
   indexView = TemplateFactory.instance.getTemplate("main/index_view.html")
   indexView.render(
     { 
       loginUser: userService.getUserInfoFromCookie(env),
       postList: response[0],      
-      pageIndexes: pageCount > 0 ? (0..pageCount).to_a : Array(Int32).new,
-      hasPages: pageCount > 0,
+      pageIndexes: pageCount > 0 ? (0...pageCount).to_a : Array(Int32).new,
+      prevPageId: (pageCount > 0) && (pageId > 1) ? prevPageId : nil,
+      nextPageId: (pageCount > 0) && (nextPageId <= pageCount) ? nextPageId : nil,
       currentPage: pageId,
       quote: quoteService.getRandomQuote()
     })
